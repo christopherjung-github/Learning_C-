150751e0cabd0b6a73bff39f795106aa0d31abcc
Christopher Jung <57968809+christopherjung-github@users.noreply.github.com>
2022-10-13 18:01:13 -0700

    Create CodeDemo.cpp

---
+++ Ch01/01_03/Begin/CodeDemo/CodeDemo.cpp
@@ -0,0 +1,11 @@
+// Learning C++ 
+// Exercise 01_03
+// Hello World, by Chris Jung
+
+#include <iostream> // standard library for c++
+
+int main() {
+    std::cout << "Hello world" << std::endl;
+
+    return(0);
+}
